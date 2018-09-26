eee9b86d98bf756236ac42186c57134cede37820
V <vzmon1@gmail.com>
2018-09-26 19:19:39 -0400

    9/26  7:19 PM Modified SearchResult to be read only

--- ViewModel/SearchResults.cs
+++ ViewModel/SearchResults.cs
@@ -2,14 +2,14 @@ using System;
 namespace stackoverflowclone.ViewModel
 {
     public class SearchResults{
-        public int Id { get; set; }
-        public string Answer { get; set; }
-        public string Question { get; set; }
+        public int Id { get;  }
+        public string Answer { get;  }
+        public string Question { get;  }
 
         public DateTime createDate {get;}
         
-        public int upVote { get; set; }
+        public int upVote { get;  }
 
-        public int downVote { get; set; }
+        public int downVote { get;  }
     }
 }
