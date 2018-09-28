396e85d3dfcfc48db4a5c1f8624668ab67e1a9d7
V <vzmon1@gmail.com>
2018-09-28 07:29:50 -0400

    Update 9/28 7:29 AM

--- server/ViewModel/SearchResults.cs
+++ server/ViewModel/SearchResults.cs
@@ -2,14 +2,14 @@ using System;
 namespace stackoverflowclone.ViewModel
 {
     public class SearchResults{
-        public int Id { get;  }
-        public string Answer { get;  }
-        public string Question { get;  }
+        public int Id { get; set; }
+        public string Answer { get;  set;}
+        public string Question { get; set; }
 
-        public DateTime createDate {get;}
+        public DateTime createDate {get; set;}
         
-        public int upVote { get;  }
+        public int upVote { get; set; }
 
-        public int downVote { get;  }
+        public int downVote { get;  set;}
     }
 }
